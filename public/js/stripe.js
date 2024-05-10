241baec04ac9c60ded2c97fb6561b8b1e2cce50a
Bhavya Luthra <123308266+BhavyaLuthra18@users.noreply.github.com>
2024-05-10 19:48:54 +0530

    Updated stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookTour = async tourId => {
   try {
     //1)  Get the checkout session from the API
     const session = await axios(
-      `http://127.0.0.1:8584/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`
     );
     console.log(session);
     //2) Create checkout form + charge credit card
