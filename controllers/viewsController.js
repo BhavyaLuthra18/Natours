969abef13d902c5b4f6887f041ec6689606b6f9f
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2024-05-11 20:46:52 +0530

    Fix bugs

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -6,6 +6,7 @@ const AppError = require('../utilis/appError');
 
 exports.alerts = (req, res, next) => {
   const { alert } = req.query;
+
   if (alert === 'booking')
     res.locals.alert =
       "Your booking was successful! Please check your email for a confirmation. if your booking doesn't show up here immediatly , please come back localStorage.";
