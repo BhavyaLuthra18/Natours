47b5bb4731a032662b3a89e35569391008f6e7f0
Bhavya Luthra <bhavyaluthra1234@gmail.com>
2024-05-12 12:21:06 +0530

    FIXING

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ process.on('uncaughtException', err => {
 });
 const app = require('./app');
 
-//const uri = process.env.MONGODB_URI;
-const uri = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  encodeURIComponent(process.env.DATABASE_PASSWORD)
-);
+const uri = process.env.DATABASE;
+//const uri = process.env.DATABASE.replace(
+//  '<PASSWORD>',
+// encodeURIComponent(process.env.DATABASE_PASSWORD)
+//);
 
 mongoose
   // .connect(process.env.DATABASE_LOCAL, {
